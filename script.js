231b6aacdcc009a0bfec32e198c282ae930a0870
Yonathan Dereje <eldanadereje06@gmail.com>
2025-01-25 19:26:05 +0300

    updatinghtml

---
+++ script.js
@@ -0,0 +1,8 @@
+const main = document.querySelector('.main');
+const row1 = document.querySelector('.row-1');
+const img1 = document.querySelector('.img1');
+
+img1.addEventListener('click', () => {
+  img1.classList.add('img11');
+  img1.classList.remove('img1');
+});
