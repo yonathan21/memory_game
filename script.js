fcae168b050f98c3f3dd15032accbe3f9b3ba27e
Yonathan Dereje <eldanadereje06@gmail.com>
2025-02-08 09:43:55 +0300

    checkingifthecardismatched

--- script.js
+++ script.js
@@ -3,16 +3,30 @@ const cards = document.querySelectorAll(".card");
 let cardOne, cardTwo;
 
 function flipCard(e){
+
   let clickedCard = e.target;
   clickedCard.classList.add('flip'); 
+
   if(clickedCard !== cardOne){
     if(!cardOne){
       return cardOne = clickedCard; 
     }
     cardTwo = clickedCard;
-    console.log(cardOne, cardTwo);
+    
+    let cardOneImg = cardOne.querySelector('img').src,
+    cardTwoImg = cardTwo.querySelector('img').src;
+
+    matchcards(cardOneImg, cardTwoImg);
   }
+
+}
+
+function matchcards(img1, img2){
+  if(img1 === img2){
+    return console.log('card matched');
   }
+  console.log('not matched');
+}
 
 cards.forEach(card => {
   card.addEventListener('click', flipCard);
