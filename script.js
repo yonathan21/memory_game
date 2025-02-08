c732d705a5ec5d65f875253d74623b3030a2f061
Yonathan Dereje <eldanadereje06@gmail.com>
2025-02-08 09:31:38 +0300

    gettingdiferentcard

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const cards = document.querySelectorAll(".card");
 
+let cardOne, cardTwo;
+
 function flipCard(e){
   let clickedCard = e.target;
   clickedCard.classList.add('flip'); 
+  if(!cardOne){
+    return cardOne = clickedCard; 
+  }
+  cardTwo = clickedCard;
+  console.log(cardOne, cardTwo);
 }
 
 cards.forEach(card => {
