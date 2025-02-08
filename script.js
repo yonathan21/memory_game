3e3352ce726ae0d23c5f28568c4cb7fb96459fcf
Yonathan Dereje <eldanadereje06@gmail.com>
2025-02-08 09:35:02 +0300

    removingdoubleclick

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ let cardOne, cardTwo;
 function flipCard(e){
   let clickedCard = e.target;
   clickedCard.classList.add('flip'); 
-  if(!cardOne){
-    return cardOne = clickedCard; 
+  if(clickedCard !== cardOne){
+    if(!cardOne){
+      return cardOne = clickedCard; 
+    }
+    cardTwo = clickedCard;
+    console.log(cardOne, cardTwo);
   }
-  cardTwo = clickedCard;
-  console.log(cardOne, cardTwo);
-}
+  }
 
 cards.forEach(card => {
   card.addEventListener('click', flipCard);
